1eb713a3ba7c44e2e36a864d810b09b99bed60ad
John Nielsen <archangel@Lestat-De-Lioncourts-MB.local>
2019-07-07 21:43:09 -0700

    Update Package JSON

--- DAssistant/client/src/App.js
+++ DAssistant/client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './Card/Card';
+import Card from './components/Card/Card'
+import Button from './components/CustomButtons/Button'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Card/>
+                <Button/>
             </div>
         );
     }
